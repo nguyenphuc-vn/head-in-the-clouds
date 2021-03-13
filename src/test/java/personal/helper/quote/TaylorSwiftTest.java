ee05a0e36077dc508037fd5fea04b7c6c8c48427
Phuc <Phuc@PHUC-PC>
2021-03-13 22:14:31 +0700

    add more quote from https://api.quotable.io/random

--- src/test/java/personal/helper/quote/TaylorSwiftTest.java
+++ src/test/java/personal/helper/quote/TaylorSwiftTest.java
@@ -1,39 +1,37 @@
-package personal.helper;
+package personal.helper.quote;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import personal.helper.quote.IQuote;
-import personal.helper.quote.TaylorSwift;
 import personal.model.quote.Quote;
 import personal.model.quote.TaylorSwiftDTO;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaylorSwiftTest {
-    IQuote taylorSwift;
+    IQuote iTaylor;
     Quote taylor;
-    private final String api = "https://api.taylor.rest/";
+    String api = "https://api.taylor.rest/";
     String broken = "https://www.baeldung.com/httpclient-status";
     String author = "Taylor Swift";
-   // String anything;
+   //String anything;
     @BeforeEach
     void setUp() {
-        taylorSwift = new TaylorSwift();
+        iTaylor = new TaylorSwift();
         taylor = new TaylorSwiftDTO();
-        taylorSwift.connect(api);
-       taylor = (TaylorSwiftDTO) taylorSwift.transform(taylor);
+        iTaylor.connect(api);
+        taylor = (Quote) iTaylor.transform(taylor);
     }
     @Test
-    void doesItConnectsOk(){
-        assertTrue(taylorSwift.connect(api));
-        assertFalse(taylorSwift.connect(broken));
+    void doesItConnectOk(){
+        assertTrue(iTaylor.connect(api));
+        assertFalse(iTaylor.connect(broken));
     }
     @Test
     void willItReturnString(){
         // it failed but transform works
         // work now
-        assertEquals(taylor.getQuote(),taylor.getQuote());
         assertEquals(taylor.getAuthor(),author);
 
     }
+
 }
